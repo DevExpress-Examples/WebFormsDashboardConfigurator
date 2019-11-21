18548cfb671c2697e262adf7566999130db62701
DevExpressExampleBot <fakeEmail>
2019-11-21 16:09:21 +0300

    Source auto update [skip ci]

--- VB/WebFormsDashboardConfigurator/Global.asax.vb
+++ VB/WebFormsDashboardConfigurator/Global.asax.vb
@@ -11,6 +11,7 @@ Namespace WebFormsDashboardConfigurator
 		Inherits System.Web.HttpApplication
 
 		Protected Sub Application_Start(ByVal sender As Object, ByVal e As EventArgs)
+			ASPxDashboard.StaticInitialize()
 			DashboardConfigurator.Default.SetDashboardStorage(New DashboardFileStorage(Server.MapPath("App_Data/Dashboards")))
 			DashboardConfigurator.Default.SetDataSourceStorage(CreateDataSourceStorage())
 			DashboardConfigurator.Default.SetConnectionStringsProvider(New ConfigFileConnectionStringsProvider())
